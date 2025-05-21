f7fc9da0003d4d09be34ef8cb4ac7021a4ac4cd2
kparasch <konstantinos.paraschou@desy.de>
2025-05-21 14:38:49 +0200

    first display only simulation

---
+++ main.py
@@ -0,0 +1,113 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QPushButton, QProgressBar, QTableWidget, QSizePolicy
+from PyQt6.QtWidgets import QTableWidgetItem, QCheckBox, QButtonGroup, QGroupBox
+from PyQt6.QtCore import Qt, QThread
+#from colors import DESYCyan, DESYOrange, Dunkelblau, Gelb, Dunkelrot, Petrol, Grun, Olive
+import pyqtgraph as pg
+import numpy as np
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView
+from PyQt6.QtGui import QPen, QBrush, QColor, QPalette
+from plot_worker import get_plot_thread, References
+
+DESYCyan = (0, 159, 223)
+DESYOrange = (241, 143, 31)
+bluish = (33, 41, 70)
+DESYdarkblue = (0,75,110)
+MYCOL = DESYOrange
+
+app = QApplication([])
+window = QWidget()
+#window.setStyleSheet("background-color: rgb(0,75,110);")
+
+total_width = 1000
+total_height = 690
+# window.setStyleSheet(f'background-color: {Dunkelblau};'
+#                      f'color: {Olive};')
+window.setWindowTitle("Orbit display")
+window.setGeometry(100, 100, total_width, total_height)
+
+
+layout = QGridLayout(parent=window)
+window.setLayout(layout)
+
+
+plot_thread = get_plot_thread()
+worker = plot_thread.worker
+
+button_plot = QPushButton('Plot', parent=window) 
+button_plot.clicked.connect(lambda : worker.bars_h.setOpts(height=np.random.random(worker.N)*10-5))
+button_plot.clicked.connect(lambda : worker.bars_v.setOpts(height=np.random.random(worker.N)*10-5))
+
+
+button_start = QPushButton('Start', parent=window) 
+button_start.clicked.connect(plot_thread.start)
+
+button_stop = QPushButton('Stop', parent=window) 
+button_stop.clicked.connect(plot_thread.requestInterruption)
+
+
+references = References()
+
+button_save = QPushButton('Save reference', parent=window) 
+button_save.clicked.connect(lambda : references.save_reference(worker))
+
+group_display_box = QGroupBox(parent=window)
+group_display = QButtonGroup(parent=window)
+checkbox_orbit = QCheckBox('Orbit', parent=window)
+checkbox_ref1 = QCheckBox('Orbit - Reference 1', parent=window)
+checkbox_ref2 = QCheckBox('Orbit - Reference 2', parent=window)
+group_display.addButton(checkbox_orbit)
+group_display.addButton(checkbox_ref1)
+group_display.addButton(checkbox_ref2)
+checkbox_orbit.setChecked(True)
+
+checkbox_orbit.checkStateChanged.connect(lambda : references.switch_to_no_ref(checkbox_orbit.isChecked(), worker))
+checkbox_ref1.checkStateChanged.connect(lambda : references.switch_to_ref1(checkbox_ref1.isChecked(), worker))
+checkbox_ref2.checkStateChanged.connect(lambda : references.switch_to_ref2(checkbox_ref2.isChecked(), worker))
+
+class GroupSave(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.group = QButtonGroup(parent=self)
+        self.box1 = QCheckBox('to Reference 1', parent=self)
+        self.box2 = QCheckBox('to Reference 2', parent=self)
+
+        self.group.addButton(self.box1)
+        self.group.addButton(self.box2)
+
+        self.layout = QGridLayout(parent=self)
+        self.layout.addWidget(self.box1, 0, 1)
+        self.layout.addWidget(self.box2, 0, 2)
+        self.box1.setChecked(True)
+
+group_save = GroupSave(parent=window)
+group_save.box1.checkStateChanged.connect(lambda : references.set_ref_to_save(group_save.box1.isChecked(), '1'))
+group_save.box2.checkStateChanged.connect(lambda : references.set_ref_to_save(group_save.box2.isChecked(), '2'))
+
+# checkbox_orbit.clicked.connect(checkbox_orbit.setDisabled)
+# checkbox_orbit.clicked.connect(checkbox_ref1.setEnabled)
+# checkbox_orbit.clicked.connect(checkbox_ref2.setEnabled)
+# checkbox_orbit.clicked.connect(checkbox_ref1.)
+# 
+# checkbox_ref1.clicked.connect(checkbox_orbit.setEnabled)
+# checkbox_ref1.clicked.connect(checkbox_ref1.setDisabled)
+# checkbox_ref1.clicked.connect(checkbox_ref2.setEnabled)
+# 
+# checkbox_ref2.clicked.connect(checkbox_orbit.setEnabled)
+# checkbox_ref2.clicked.connect(checkbox_ref1.setEnabled)
+# checkbox_ref2.clicked.connect(checkbox_ref2.setDisabled)
+
+layout.addWidget(plot_thread.worker.plot_h, 0, 0, 1, 3)
+layout.addWidget(plot_thread.worker.plot_v, 1, 0, 1, 3)
+
+layout.addWidget(button_plot, 2, 0, 1, 1)
+layout.addWidget(button_start, 2, 1, 1, 1)
+layout.addWidget(button_stop, 2, 2, 1, 1)
+layout.addWidget(button_save, 3, 0, 1, 1)
+layout.addWidget(checkbox_orbit, 4, 0, 1, 1)
+layout.addWidget(checkbox_ref1, 5, 0, 1, 1)
+layout.addWidget(checkbox_ref2, 6, 0, 1, 1)
+layout.addWidget(group_save, 3, 1, 1, 1)
+window.show()
+sys.exit(app.exec())
---
+++ plot_worker.py
@@ -0,0 +1,199 @@
+from PyQt6.QtCore import pyqtSignal, QObject, QThread, QCoreApplication, QTimer, pyqtSlot
+import pyqtgraph as pg
+import numpy as np
+import time
+DESYCyan = (0, 159, 223)
+DESYOrange = (241, 143, 31)
+cool = (8, 247, 254)
+bluish = (33, 41, 70)
+pink = (254, 83, 187)
+DESYdarkblue = (0,75,110)
+MYCOL = DESYOrange
+
+xticks =  [[21.5, 'W'], [45.5 ,'NW'], [67.5, 'N'], [98.5, 'NO'], 
+           [172.5, 'O'], [205.5, 'SO'], [224.5, 'S'], [245.5, 'SWL']]
+
+
+def bar_plot(graph, x, y):
+    #graph.curve.setData(y)
+    graph.clear()
+    for xpos, xlabel in xticks:
+        graph.addItem(pg.InfiniteLine(pos=xpos, angle=90))
+    graph.getPlotItem().getAxis('bottom').setTicks([xticks])
+    graph.addItem(pg.BarGraphItem(x=x, height=y, width=0.5, pen=MYCOL, brush=MYCOL))
+
+class References(object):
+    def __init__(self):
+        self.bpm_list = [line.strip('\n,\'') for line in open('bpm_list.txt','r').readlines()]
+        self.N = len(self.bpm_list) 
+
+        self.zero = np.zeros(self.N)
+
+        self.reference_1_h = np.zeros(self.N)
+        self.reference_1_v = np.zeros(self.N)
+
+        self.reference_2_h = np.zeros(self.N)
+        self.reference_2_v = np.zeros(self.N)
+        
+        self.ref_to_save = '1'
+
+    def get_and_stop_thread(self, worker):
+        thread = worker.thread()
+        if thread.isRunning():
+            worker.finished.emit()
+            thread.wait()
+            restart_thread = True
+        else:
+            restart_thread = False
+        return thread, restart_thread
+
+    def switch_to_no_ref(self, flag, worker):
+        if flag:
+            thread, restart_thread = self.get_and_stop_thread(worker)
+
+            orbit_h = worker.bars_h.opts['height'] + worker.reference_h
+            orbit_v = worker.bars_v.opts['height'] + worker.reference_v
+
+            worker.reference_h = self.zero
+            worker.reference_v = self.zero
+
+            worker.bars_h.setOpts(height=orbit_h - worker.reference_h)
+            worker.bars_v.setOpts(height=orbit_v - worker.reference_v)
+
+            if restart_thread:
+                thread.start()
+
+    def switch_to_ref1(self, flag, worker):
+        if flag:
+            thread, restart_thread = self.get_and_stop_thread(worker)
+
+            orbit_h = worker.bars_h.opts['height'] + worker.reference_h
+            orbit_v = worker.bars_v.opts['height'] + worker.reference_v
+
+            worker.reference_h = self.reference_1_h
+            worker.reference_v = self.reference_1_v
+
+            worker.bars_h.setOpts(height=orbit_h - worker.reference_h)
+            worker.bars_v.setOpts(height=orbit_v - worker.reference_v)
+            print(np.std(orbit_h), np.std(worker.reference_h), np.std(orbit_h - worker.reference_h))
+            if restart_thread:
+                thread.start()
+
+    def switch_to_ref2(self, flag, worker):
+        if flag:
+            thread, restart_thread = self.get_and_stop_thread(worker)
+
+            orbit_h = worker.bars_h.opts['height'] + worker.reference_h
+            orbit_v = worker.bars_v.opts['height'] + worker.reference_v
+            
+            worker.reference_h = self.reference_2_h
+            worker.reference_v = self.reference_2_v
+
+            worker.bars_h.setOpts(height=orbit_h - worker.reference_h)
+            worker.bars_v.setOpts(height=orbit_v - worker.reference_v)
+
+            if restart_thread:
+                thread.start()
+
+    def set_ref_to_save(self, flag, ref_to_save: str):
+        if flag:
+            self.ref_to_save = ref_to_save
+    
+    def save_reference(self, worker):
+        thread, restart_thread = self.get_and_stop_thread(worker)
+
+        print('trying to save', self.ref_to_save)
+        if self.ref_to_save == '1':
+            print('saving 1')
+            self.reference_1_h = worker.bars_h.opts['height'] + worker.reference_h
+            self.reference_1_v = worker.bars_v.opts['height'] + worker.reference_v
+        elif self.ref_to_save == '2':
+            self.reference_2_h = worker.bars_h.opts['height'] + worker.reference_h
+            self.reference_2_v = worker.bars_v.opts['height'] + worker.reference_v
+        else:
+            raise Exception(f'Unknown reference: {self.ref_to_save}')
+        
+        if restart_thread:
+            thread.start()
+
+    
+
+class Plot_worker(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
+    
+    def __init__(self):
+        super().__init__()
+        self.plot_h = pg.plot()
+        self.plot_v = pg.plot()
+
+        self.bpm_list = [line.strip('\n,\'') for line in open('bpm_list.txt','r').readlines()]
+        self.N = len(self.bpm_list) 
+        self.ymin=-100
+        self.ymax=100
+
+        self.plot_h.setYRange(self.ymin, self.ymax)
+        self.plot_v.setYRange(self.ymin, self.ymax)
+
+        self.bars_h = pg.BarGraphItem(x=np.arange(self.N), height=np.zeros(self.N), width=0.5, pen=MYCOL, brush=MYCOL)
+        self.bars_v = pg.BarGraphItem(x=np.arange(self.N), height=np.zeros(self.N), width=0.5, pen=MYCOL, brush=MYCOL)
+
+        self.plot_h.addItem(self.bars_h)
+        self.plot_v.addItem(self.bars_v)
+
+        self.plot_h.setLabel('left', 'Horizontal', units='mm')
+        self.plot_v.setLabel('left', 'Vertical', units='mm')
+
+        self.plot_h.setBackground(DESYdarkblue)
+        self.plot_v.setBackground(DESYdarkblue)
+
+        self.reference_h = np.zeros(self.N)
+        self.reference_v = np.zeros(self.N)
+
+        print('Plot worker initialized')
+
+
+
+    @pyqtSlot()
+    def plot(self):
+        bar_plot(self.plot_h, np.arange(self.N), 100*np.sin(np.arange(self.N)) + np.random.random(self.N)*10-5)
+        bar_plot(self.plot_v, np.arange(self.N), 100*np.sin(np.arange(self.N)) + np.random.random(self.N)*10-5)
+
+    @pyqtSlot()
+    def check_interruption(self):
+        if self.thread().isInterruptionRequested():
+            #self.thread().timer.stop()
+            print('Plot thread has been requested to interrupt.')
+            self.finished.emit()
+            return
+
+    @pyqtSlot()
+    def start_plotting(self):
+        #self.selectedBPMs.setEnabled(False)
+        print('Starting to_plot!')
+        self.thread().timer.start()
+
+
+def get_plot_thread():
+    plot_thread = QThread()
+    print('Plot thread initialized')
+    worker = Plot_worker()
+    worker.moveToThread(plot_thread)
+    timer = QTimer()
+    timer.moveToThread(plot_thread)
+    plot_thread.started.connect(worker.start_plotting)
+    worker.finished.connect(plot_thread.quit)
+    plot_thread.destroyed.connect(lambda : print('Plot thread destroyed'))
+    worker.destroyed.connect(lambda : print('Plot worker destroyed'))
+    timer.destroyed.connect(lambda : print('Plot timer destroyed'))
+    plot_thread.worker = worker ### attach worker to thread so that it is not garbage collected
+    plot_thread.timer = timer ### attach timer to thread so that it is not garbage collected
+    #timer.timeout.connect(lambda : bar_plot(worker.plot_h, np.arange(N), 100*np.sin(np.arange(N)) + np.random.random(N)*10-5))
+    timer.timeout.connect(lambda : worker.bars_h.setOpts(height=100*np.sin(np.arange(worker.N)) + np.random.random(worker.N)*10-5 - worker.reference_h))
+    timer.timeout.connect(lambda : worker.bars_v.setOpts(height=100*np.sin(np.arange(worker.N)) + np.random.random(worker.N)*10-5 - worker.reference_v))
+    #timer.timeout.connect(lambda : bar_plot(worker.plot_v, np.arange(N), 100*np.sin(np.arange(N)) + np.random.random(N)*10-5))
+    #timer.timeout.connect(lambda : bar_plot(worker.plot_v, np.arange(N), 100*np.sin(np.arange(N)) + np.random.random(N)*10-5))
+    timer.timeout.connect(worker.check_interruption)
+    timer.setInterval(50)
+    plot_thread.finished.connect(timer.stop)
+    return plot_thread
